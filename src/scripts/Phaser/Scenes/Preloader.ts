e65542070073b7bb754affce83bf1e2a2ad255db
HomoHikka <halfideas31@gmail.com>
2022-10-18 18:48:19 +0300

    Update Preloader

--- src/scripts/Phaser/Scenes/Preloader.ts
+++ src/scripts/Phaser/Scenes/Preloader.ts
@@ -2,29 +2,63 @@ import 'phaser';
 import { ImageKeys } from '../Keys/ImageKeys';
 import { SceneKeys } from './SceneKeys';
 
+interface Asset {
+	type: 'json' | 'image' | 'svg';
+	key: ImageKeys;
+	url: string
+}
+
+interface CustomAsset {
+	type: 'font';
+	key: ImageKeys;
+	url: string
+}
+
 export default class Preloader extends Phaser.Scene {
+	protected _customCompleted = false;
+	protected _loaderCompleted = false;
+
 	constructor() {
 		super(SceneKeys.Preloader);
 	}
 
-	public loadFont(name: string, url: string): void {
+	public async loadFont(name: string, url: string): Promise<void> {
 		const newFont = new FontFace(name, `url(${url})`);
-		newFont.load().then(function (loaded) {
-			document.fonts.add(loaded);
-		}).catch(function (error) {
-			return error;
-		});
+		const font = await newFont.load();
+
+		document.fonts.add(font);
 	}
 
 	public preload(): void {
-		this.loadFont('monogram', 'assets/monogram-extended.ttf');
-		this.load.image(ImageKeys.Button, 'assets/images/grey_button00.png');
-		this.load.svg(ImageKeys.CellBorder, 'assets/images/square-filled.svg');
-		this.load.svg(ImageKeys.Cross, 'assets/images/cross-svgrepo-com.svg');
-		this.load.svg(ImageKeys.Circle, 'assets/images/circle-regular.svg');
+		this.load.json('assets', 'assets/assets.json');
+		this.load.json('customAssets', 'assets/customAssets.json');
 	}
 
 	public create(): void {
-		this.scene.start(SceneKeys.Menu);
+		const assets: Asset[] = this.cache.json.get('assets');
+		const customAssets: CustomAsset[] = this.cache.json.get('customAssets');
+
+		assets.forEach((asset: Asset) => this.load[asset.type](asset.key, 'assets/' + asset.url));
+
+		this.load.start();
+		this.load.addListener(Phaser.Loader.Events.COMPLETE, () => {
+			this._loaderCompleted = true;
+			this.checkIfComplete();
+		});
+
+		Promise.all(customAssets.map((asset: CustomAsset) => {
+			switch (asset.type) {
+				case 'font':
+					return this.loadFont(asset.key, 'assets/' + asset.url);
+			}
+		})).then(() => {
+			this._customCompleted = true
+			this.checkIfComplete();
+		});
+	}
+
+	public checkIfComplete() {
+		if (this._customCompleted && this._loaderCompleted)
+			this.scene.start(SceneKeys.Menu);
 	}
 }
